b00be069951029c3186579f53a9c31ea11e7dd16
Chandler Dale <chandlerdale21@gmail.com>
2023-08-04 22:52:49 +0300

    nav bad w/css

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+require("dotenv").config({ path: "./config/.env" });
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+  res.render("login");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`server is running on ${process.env.PORT}`);
+});
