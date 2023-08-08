06f0284e77bec25cf5a05cb769727f85e808fbe6
Chandler Dale <chandlerdale21@gmail.com>
2023-08-08 23:10:47 +0300

    start of controller setup

--- server.js
+++ server.js
@@ -5,8 +5,13 @@ require("dotenv").config({ path: "./config/.env" });
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
   res.render("login");
+});
+
+app.get("/signup", (req, res) => {
+  res.render("signup");
 });
 
 app.listen(process.env.PORT, () => {
